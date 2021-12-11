0c00b2963b4bf478e4a103dcfde56214a4080309
ishishkin <shishkin.ia16@physics.msu.ru>
2021-12-11 21:31:12 +0300

    Add normal noise

---
+++ lib/ishishkin/diploma/models.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class LinearFunction:
+
+    def __init__(self, *, a_0=0, a_1=1):
+        self.a_0 = a_0
+        self.a_1 = a_1
+
+    def get_y(self, x):
+        return self.a_0 + self.a_1*x
+
+    def make_x(self, start=0, stop=10, step=0.5):
+        return np.array(np.arange(start, stop, step))
+
+    def make_true_y(self, start=0, stop=10, step=0.5):
+
+        x_arr = self.make_x(start, stop, step)
+        y_arr = self.get_y(x_arr)
+
+        return y_arr
+
+    def uniform_noise(self, start=0, stop=10, step=0.5, low=-1, high=1):
+        x_arr = self.make_x(start, stop, step)
+        noise = np.random.uniform(low, high, x_arr.shape)
+        y_arr_noise = self.get_y(x_arr) + noise
+        intervals = np.ones(x_arr.shape) * (low+high)/2
+
+        return y_arr_noise, intervals
+
+    def normal_noise(self, start=0, stop=10, step=0.5, loc=0, scale=1):
+
+        x_arr = self.make_x(start, stop, step)
+        noise = np.random.normal(loc, scale, x_arr.shape)
+        y_arr_noise = self.get_y(x_arr) + noise
+        intervals = np.ones(x_arr.shape)*scale*3.5
+
+
+
+        return y_arr_noise, intervals
